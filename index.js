9cc38bf777036a531106ed5aea940bc02dc38e70
Will <willhy@gmail.com>
2015-03-08 18:21:10 -0500

    Add JS handling for contact form.

---
+++ index.js
@@ -0,0 +1,29 @@
+(function () {
+  $form = $('form');
+
+  function submit() {
+    var data = $form.serialize();
+
+    // Yeah, I know this is insecure, but leave it be, please.  I'll buy you a beer.
+    sendEmail(data);
+
+    return false;
+  }
+
+  function sendEmail(data) {
+    $.post({
+      url:  '',
+      data: data
+    }).done(showConfirmation).fail(showError);
+  }
+
+  function showConfirmation() {
+    alert('Thank you.  Your email has been sent.');
+  }
+
+  function showError() {
+    alert('There was an error.  Please try again later.');
+  }
+
+  $form.on('submit', submit);
+})();
